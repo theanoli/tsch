31ce535aca2400ead2c92acc91fc090df68fc84d
theanoli <theanosaurus@gmail.com>
2017-01-27 19:04:49 -0700

    It works yay

--- dpdk_setup.sh
+++ dpdk_setup.sh
@@ -2,21 +2,26 @@
 # are done from the setup.sh file in DPDK's tools directory (see the tasks
 # array below). 
 #
-# Must include as arguments
-#	1. which Emulab machine type you are using (for hugepage mappings)
-# 	2. the interface you want to activate DPDK on.
-#
-# Usage: 
+# Must include as argument the interface you want to activate DPDK on. Usage: 
 #	sudo bash dpdk_setup.sh machine_type iface_name  
+#
+# Note the setup.sh program is extremely sensitive to misplaced newlines!
+# If it seems to be working incorrectly, echo $options to see what you're
+# piping and make sure it is exactly as you'd manually enter it to the program.
 #
 # CD into the directory where DPDK setup lives, or else this will not work
 # because of this line:
 dpdk_dir=`echo pwd`
 
-node_type=$1
-iface=$2
+iface=$1
 
-tasks=( "x86_64-native-linuxapp-gcc" "Insert IGB UIO module" "Setup hugepage mappings for NUMA systems" "Bind Ethernet device to IGB UIO module" )
+# Get the number of NUMA nodes for this machine type
+num_numa_nodes=$( lscpu | awk -F': +' '/NUMA node\(s\)/ { print $2 }' )
+
+tasks=( #"x86_64-native-linuxapp-gcc" 
+	"Insert IGB UIO module" 
+	"Setup hugepage mappings for NUMA systems" 
+	"Bind Ethernet device to IGB UIO module" )
 
 # Files in which to dump options menu of the DPDK setup script (fname)
 # and the PCI information (pci_fname)
@@ -26,10 +31,9 @@ pci_fname=another_random_file.txt
 # Dump the menu contents; "q" will quit, since we don't know exit code yet
 printf q | bash setup.sh > $fname
 
-optios=""
-
 # Extract the option number from the setup menu text
 for i in "${tasks[@]}"; do
+	echo Working on $i...
 	# Find the correct line and extract the option number
 	optnum=$(awk -v pat="$i" '$0 ~ pat { 
 				gsub(/\[/,""); 
@@ -38,31 +42,29 @@ for i in "${tasks[@]}"; do
 			}' $fname )	
 	
 	# "Bind Ethernet device..." is a special case; we need to examine
-	# a sub-menu, too
+	# a sub-menu, too. This is ugly but it works and faster than 
+	# alternative (lshw is really slow!)
+	echo "$optnum"
 	if [ "$i" == "Bind Ethernet device to IGB UIO module" ]; then
 		# Expand the submenu and save to file; printf preserves \n
-		printf "$optnum\n\nq\n\n" | bash setup.sh > $pci_fname
+		printf "$optnum\nq\nq\nq\n" | bash setup.sh > $pci_fname
 
-		optnum="$optnum\n$(awk -v pat="$iface" '$0 ~ pat { 
-					gsub(/\[/,""); 
-					gsub(/\]/,""); 
-					print $1 
-				}' $pci_fname )"	
+		optnum="$optnum\n$(awk -v pat="$iface" '$0 ~ pat { print $1 }' $pci_fname | 
+				awk -F':' '{ print $2":"$3 }')"
 	fi
 	
-	# Number of times you need to enter mappings depends on node type
+	# Add an input for every NUMA node
 	if [ "$i" == "Setup hugepage mappings for NUMA systems" ]; then
-		if [ "$node_type" == "d430" ]; then
-			numa_string="1024\n1024"
-		else
-			numa_string="1024\n1024\n1024\n1024"
-		fi
-		optnum="$optnum\n$numa_string"
+		for i in `seq 1 $num_numa_nodes`; do
+			numa_string="$numa_string\n1024"
+		done
+
+		optnum="$optnum$numa_string"
 	fi
 
 	options="$options$optnum\n\n"
 done
 
-echo $options
+printf "$options q\n" | bash setup.sh
 rm $fname
 rm $pci_fname
