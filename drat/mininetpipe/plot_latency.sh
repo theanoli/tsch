8426d9763670075502f48534ef2e0f4440ce181f
theanoli <theanosaurus@gmail.com>
2017-06-07 17:46:49 -0600

    fix stupid bug in plotting software

--- drat/mininetpipe/plot_latency.sh
+++ drat/mininetpipe/plot_latency.sh
@@ -3,8 +3,10 @@
 # Delete padding spaces
 file="results/`ls ./results | sort | head -1`"
 cat $file | tr -d " "
+
+# Remove file extension from filename
 expname=${file%*.*}
 
 echo $expname
-gnuplot -e "expname='$fname'" plot_latency.p
+gnuplot -e "expname='$expname'" plot_latency.p
 eog $expname.png
