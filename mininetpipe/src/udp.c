0708ee1d3da815bdf489d1a9270bb9eef8093ef9
theanoli <theanosaurus@gmail.com>
2017-06-14 18:07:52 -0600

    UDP works

--- mininetpipe/src/udp.c
+++ mininetpipe/src/udp.c
@@ -1,5 +1,7 @@
 #include "harness.h"
 
+int doing_reset = 0;
+
 void
 Init (ArgStruct *p, int *pargc, char ***pargv)
 {
@@ -9,15 +11,14 @@ Init (ArgStruct *p, int *pargc, char ***pargv)
 void
 Setup (ArgStruct *p)
 {
-    p->s_str = (char *) malloc (PSIZE);
-    p->r_str = (char *) malloc (PSIZE);
+    p->s_ptr = (char *) malloc (PSIZE);
+    p->r_ptr = (char *) malloc (PSIZE);
 
     if ((p->s_ptr == NULL) || (p->r_ptr == NULL)) {
         printf ("Malloc error!\n");
         exit (1);
     }
 
-    int one = 1;
     int sockfd;
     struct sockaddr_in *lsin1, *lsin2;
     char *host;
@@ -33,7 +34,7 @@ Setup (ArgStruct *p)
     memset ((char *) lsin2, 0, sizeof (*lsin2));
 
     if ((sockfd = socket (socket_family, SOCK_DGRAM, 0)) < 0) {
-        printf ("tester: can't open stream socket! errno=%d\n", errno);
+        perror ("tester: can't open stream socket!");
         exit (-4);
     }
 
@@ -45,7 +46,7 @@ Setup (ArgStruct *p)
     // Don't need to set NODELAY; no such thing in UDP
     
     if (p->tr) {
-        // Sender side
+        // Sender side: get info about the server
         if (atoi (host) > 0) {
             lsin1->sin_family = AF_INET;
             lsin1->sin_addr.s_addr = inet_addr (host);
@@ -62,7 +63,7 @@ Setup (ArgStruct *p)
 
         }
 
-        lsin->sin_port = htons (p->port);
+        lsin1->sin_port = htons (p->port);
         p->commfd = sockfd;
 
     } else if (p->rcv) {
@@ -73,13 +74,13 @@ Setup (ArgStruct *p)
         lsin1->sin_port         = htons (p->port);
 
         if (bind (sockfd, (struct sockaddr *) lsin1, sizeof (*lsin1)) < 0) {
-            printf ("tester: server: bind on local address failed!"
-                    "errno=%d", errno);
+            perror ("tester: server: bind on local address failed!"
+                    "errno");
             exit (-6);
         }
 
         p->servicefd = sockfd;
-        printf ("Listening on socket %d...\n", sockfd);
+        printf ("Bound to socket %d...\n", sockfd);
 
     }
 
@@ -92,32 +93,39 @@ void
 SendData (ArgStruct *p)
 {
     int bytesWritten, bytesLeft;
-    char *q;
+    char *q = NULL;
     struct timespec sendtime;
+    struct sockaddr_in *remote = NULL;
+    socklen_t len;
 
     bytesLeft = p->bufflen;
     bytesWritten = 0;
 
     if (p->tr) {
+        remote = &(p->prot.sin1);
         sendtime = When2 ();
         snprintf (p->s_ptr, PSIZE, "%lld, %.9ld%-31s",
                 (long long) sendtime.tv_sec, sendtime.tv_nsec, ",");
         q = p->s_ptr;
-    } else {
+    } else if (p->rcv) {
+        remote = &(p->prot.sin2);
         q = p->r_ptr;
     }
 
     p->bufflen = strlen (q);
     bytesLeft = p->bufflen;
+    len = sizeof (*remote);
 
     while ((bytesLeft > 0) &&
-            (bytesWritten = send (p->commfd, q, bytesLeft, 0)) > 0) {
+            (bytesWritten = sendto (p->commfd, q, bytesLeft, 0, 
+                                    (struct sockaddr *) remote, 
+                                    len)) > 0) {
         bytesLeft -= bytesWritten;
         q += bytesWritten;
     }
     
     if (bytesWritten == -1) {
-        printf ("tester: write: error encountered, errno=%d\n", errno);
+        perror ("tester: send: error encountered");
         exit (401);
     }
 }
@@ -130,13 +138,20 @@ RecvData (ArgStruct *p)
     int bytesRead;
     char *q;
     char *buf;
+    struct sockaddr_in *remote;
+    socklen_t len;
 
     bytesRead = 0;
     q = p->r_ptr;
     bytesLeft = PSIZE - 1;
 
+    remote = &(p->prot.sin2);
+    len = sizeof (*remote);
+
     while ((bytesLeft > 0) &&
-           (bytesRead = recv (p->commfd, q, bytesLeft, 0)) > 0) {
+           (bytesRead = recvfrom (p->commfd, q, bytesLeft, 0, 
+                                  (struct sockaddr *) remote, 
+                                  &len)) > 0) {
         bytesLeft -= bytesRead;
         q += bytesRead;
     }
@@ -145,7 +160,7 @@ RecvData (ArgStruct *p)
         printf ("tester: \"end of file\" encountered on reading from socket\n");
 
     } else if (bytesRead == -1) {
-        printf ("tester: read: error encountered, errno=%d\n", errno);
+        perror ("tester: recv: error encountered");
         exit (401);
     }
 
@@ -170,11 +185,9 @@ RecvData (ArgStruct *p)
 void
 establish (ArgStruct *p)
 {
-    int one = 1;
-    socklen_t clen;
     struct protoent *proto;
+    int one = 1;
 
-    clen = (socklen_t) sizeof (p->prot.sin2);
 
     if (p->tr) {
         // Don't need to do anything for UDP
@@ -194,42 +207,21 @@ establish (ArgStruct *p)
         // For UDP, this will let multiple processes to bind to the same port;
         // any messages sent to that port will be delivered to all processes
         if (setsockopt (p->commfd, SOL_SOCKET, SO_REUSEADDR, &one, sizeof (int))) {
-            printf ("tester: server: unable to setsockopt! errno=%d\n", errno);
+            perror ("tester: server: unable to setsockopt!");
             exit (557);
         }
     }
 
-    printf ("Connection established!\n");
+    printf ("Established the connection...\n");
 }
 
 void 
 CleanUp (ArgStruct *p)
 {
-   char *quit = "QUIT";
-   int q;
-
    printf ("Quitting!\n");
 
-   if (p->tr) {
+   close (p->commfd);
 
-      q = send (p->commfd, quit, 5, 0);
-      q = recv (p->commfd, quit, 5, 0);
-      close (p->commfd);
-
-   } else if (p->rcv) {
-
-      q = recv (p->commfd, quit, 5, 0);
-      q = send (p->commfd, quit, 5, 0);
-      close (p->commfd);
-      close (p->servicefd);
-
-   }
-
-   // Shut up the compiler
-   if (q > 0) 
-       return;
-   else
-       return;
 }
 
 
