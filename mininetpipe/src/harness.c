c084802055a5209cc6a8c99c2e32c53f1a83dea0
theanoli <theanosaurus@gmail.com>
2017-08-16 16:37:35 -0600

    delimit throughput result printout

--- mininetpipe/src/harness.c
+++ mininetpipe/src/harness.c
@@ -158,7 +158,7 @@ main (int argc, char **argv)
 
             Echo (&args);
 
-            printf ("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n");
+            printf ("\n++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n");
             printf ("Received %" PRIu64 " packets in %f seconds\n", 
                     args.counter, args.duration);
             printf ("Throughput is %f pps\n", args.counter/args.duration);
